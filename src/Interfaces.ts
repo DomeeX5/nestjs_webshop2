a47f6063ee2087e1f61f8b7ee8785dc07d3d3a99
Major Dominik <major.dominik0505@gmail.com>
2024-01-27 23:46:05 +0100

    changing between tabs

---
+++ src/Interfaces.ts
@@ -0,0 +1,11 @@
+export class UserLogin {
+    username: string;
+    password: string;
+}
+
+export class UserRegister {
+    email: string;
+    username: string;
+    password: string;
+    password_again: string;
+}
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Get, Post, Render } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import * as mysql from 'mysql2'; 
+import { UserLogin, UserRegister } from './Interfaces';
+import { Response } from 'express';
 
 const conn = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
@@ -27,14 +29,42 @@ export class AppController {
   async login() {
   }
 
-  @Post('/login')
-  async loginForm(){
-    const [ adatok ] = await conn.execute('SELECT * from users WHERE users.username = ?, users.password = ?' [login, password]) 
+  @Post('/loginSuccess')
+  @Render('login')
+  async loginForm(@Body() existingUser: UserLogin, @Res() res: Response){
+    const errors: string[] = [];
+    const [ success ] = await conn.execute('SELECT * from users WHERE username = ?, password = ?' [existingUser.username, existingUser.password]) 
+    res.redirect('/')
   }
 
   @Get('/register')
   @Render('register')
   async register() {
 
+  }
+
+  @Post('/register')
+  @Render('register')
+  async registerForm(@Body() newUser: UserRegister, @Res() res: Response){
+    const errors: string[] = [];
+    const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    const regexUsername = /^\w{1,10}$/;
+    const regexPassword = /[a-zA-Z0-9]/;
+    if(!regexEmail.test(newUser.email)){
+      errors.push('Az email-cím helytelen!')
+    }
+    if(!regexUsername.test(newUser.username)){
+      errors.push('A felhasználó név maximum 10 karakter lehet!')
+    }
+    if(newUser.password.length < 8 || !regexPassword.test(newUser.password)){
+      errors.push('A jelszó minimum 8 karakter kell legyen, és kell tartalmazzon nagybetűt, és számot!')
+    }
+    if(newUser.password_again !== newUser.password){
+      errors.push('A két jelszó nem egyezik!')
+    } else {
+      const [ adatok ] = await conn.execute('INSERT INTO users (email, username, password) VALUES (?,?,?)', [newUser.email, newUser.username, newUser.password]) 
+      res.redirect('/login');
+      return { registerSuccess: adatok }
+    }
   }
 }
